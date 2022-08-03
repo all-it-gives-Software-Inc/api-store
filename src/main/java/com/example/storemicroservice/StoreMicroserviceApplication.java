1bb0002a8fb00275bb9af24277a00512c065aa7d
ritzare <joaovictor-fza@hotmail.com>
2022-08-03 06:50:27 -0300

    config eureka

--- src/main/java/com/example/storemicroservice/StoreMicroserviceApplication.java
+++ src/main/java/com/example/storemicroservice/StoreMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.example.storemicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StoreMicroserviceApplication {
 
     public static void main(String[] args) {
